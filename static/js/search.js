b295d401722a33d49bb190ba501cb69572206800
longd3 <izzy3ds@gmail.com>
2014-10-23 16:34:56 -0700

    v2.5
    - Server-side: Create FetchUserAPI and UserDashboardHandler
    - Client-side: Improve /user_dashboard
    - Others: Rearrange locations of static files

--- static/js/search.js
+++ static/js/search.js
@@ -1,153 +1,128 @@
 /** @jsx React.DOM */
-
-var UserPlatformRow = React.createClass({
-    render: function() {
-        return (<tr><th>{this.props.platform}</th></tr>);
-    }
-});
+// The above declaration must remain intact at the top of the script.
 
 var UserRow = React.createClass({
     render: function() {
-        var name = !this.props.user.internal ?
-            this.props.user.name :
-            <span style={{color: 'orange'}}>
-                {this.props.user.name}
-            </span>;
-        return (
-            <tr>
-            	<td>{this.props.user.id}</td>
-                <td>{name}</td>
-                <td>{this.props.user.email}</td>
-                <td>{this.props.user.gender}</td>
-                <td>{this.props.user.dob}</td>
-                <td>{this.props.user.last_modified}</td>
-            </tr>
-        );
+        console.log(this.props.rowAttr, this.props.rowVal);
+        return (<tr>
+                  <td>{this.props.rowAttr}</td>
+                  <td>{this.props.rowVal}</td>    
+                </tr>)
     }
+
 });
 
-var UserTable = React.createClass({
+
+var UserProfileTable = React.createClass({
     render: function() {
-        console.log(this.props);
         var rows = [];
-        var lastPlatform = null;
-        this.props.users.forEach(function(user) {
-        	// var name_filter = user.name.indexOf(this.props.filterText) === -1 || (!user.internal && this.props.internalOnly);
-        	var email_filter = user.email.indexOf(this.props.filterText) === -1 || (!user.internal && this.props.internalOnly); 
-            if (email_filter) {
-                return;
-            }
-            if (user.platform !== lastPlatform) {
-                rows.push(<UserPlatformRow platform={user.platform} key={user.platform} />);
-            }
-            rows.push(<UserRow user={user} key={user.email} />);
-            lastPlatform = user.platform;
-        }.bind(this));
-        return (
-            <table className="table table-condensed">
-                <thead>
-                    <tr>
-                    	<th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Gender</th>
-                        <th>DOB</th>
-                        <th>Last Modified</th>
-                    </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </table>
-        );
-    }
-});
-
-var SearchBar = React.createClass({
-    handleChange: function() {
-        this.props.onUserInput(
-            this.refs.filterTextInput.getDOMNode().value,
-            this.refs.internalOnlyInput.getDOMNode().checked
-        );
-    },
-    render: function() {
-        return (
-            <form>
-            	<div className="col-md-4">
-                <input
-                	className="form-control"
-                    type="text"
-                    placeholder="Search by name"
-                    value={this.props.filterText}
-                    ref="filterTextInput"
-                    onChange={this.handleChange}
-                />
-                </div>
-                <p>
-                    <input
-                        type="checkbox"
-                        value={this.props.internalOnly}
-                        ref="internalOnlyInput"
-                        onChange={this.handleChange}
-                    />
-                    Only show internal users
-                </p>
-            </form>
-        );
-    }
-});
-
-var FilterableUserTable = React.createClass({
-    getInitialState: function() {
-        return {
-            filterText: '',
-            internalOnly: false
-        };
-    },
-    
-    handleUserInput: function(filterText, internalOnly) {
-        this.setState({
-            filterText: filterText,
-            internalOnly: internalOnly
+        console.log(this.props.profileData)
+        $.each(this.props.profileData, function(attribute, value){
+            rows.push(<UserRow rowAttr={attribute} rowVal={value} />)
         });
-    },
-    
-    render: function() {
+        var attrList = Object.keys(this.props.profileData);
+        console.log(attrList);
         return (
-            <div className="searchForm">
-                <SearchBar
-                    filterText={this.state.filterText}
-                    internalOnly={this.state.internalOnly}
-                    onUserInput={this.handleUserInput}
-                />
-                <UserTable
-                    users={this.props.users}
-                    filterText={this.state.filterText}
-                    internalOnly={this.state.internalOnly}
-                />
+            <div className="userProfile">
+            <table className="table table-condensed">
+              <thead>
+                <tr>
+                  <th>Attribute</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {rows} 
+              </tbody>
+            </table>
             </div>
         );
     }
 });
 
+var SearchForm = React.createClass({
+    getInitialState: function() {
+        return {
+            data: [{name: 'Loading...', value: ''}], 
+            email: '', 
+            success : false, 
+            failure: false, 
+            result : {}
+        }
+    },
+    fade : function() {
+        this.setState({'failure' : false, 'success': false});
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var email = this.refs.email.getDOMNode().value.trim();
+        if (!email) {
+          return;
+        }
+        console.log(email);
+        var stuff = {email: email}
+        this.setState(stuff)
+        
+        $.ajax({
+          url: this.props.url,
+          dataType: 'json',
+          type: 'GET',
+          data: stuff,
+          success: function(data) {
+            console.log(data);
+            this.setState({result: data.user_profile, success : true});
+            setTimeout(this.fade, 2000);
+          }.bind(this),
+          error: function(xhr, status, err) {
+            this.setState({
+                result: 'failed, check credentials', 
+                failure: true
+            })
+            setTimeout(this.fade, 2000);
+            console.error(this.props.url, status, err);
+          }.bind(this)
+        });
+        this.refs.email.getDOMNode().value = '';
+        return;
+    },
 
-var raw_users = [
-	{email: "iOS.Tester@sayhello.com", name: "alpha", gender: "male", height:0, weight:0, tz:-252000, last_modified:1413938179687, dob:1413938629486, id:"10674cda456f9a59dbc11fb9e33995db", internal: true, platform: 'iOS'},
-	{email: "iOS.Customer@buy.net", name: "alumina", gender: "female", height:0, weight:0, tz:-252000, last_modified:1413938049875, dob:1413938623456, id:"10674cda456f9a59dbc11fb9e33995da", internal: false, platform: 'iOS'},
-	{email: "iOS.Customer@buy.net", name: "beta", gender: "male", height:0, weight:0, tz:-252000, last_modified:1413938175345, dob:1413938624234, id:"10674cda456f9a59dbc11fb9e33995dz", internal: false, platform: 'iOS'},
-	{email: "Android.Tester@sayhello.com", name: "gamma", gender: "male", height:0, weight:0, tz:-252000, last_modified:14139381796555, dob:141393862444, id:"10674cda456f9a59dbc11fb9e33995df", internal: true, platform: 'Android'},
-	{email: "Android.Customer@buy.net", name: "delta", gender: "male", height:0, weight:0, tz:-252000, last_modified:1413938179666, dob:1413938629334, id:"10674cda456f9a59dbc11fb9e33995dc", internal: false, platform: 'Android'}
-];
+    render: function() {
 
-var USERS = _.map(raw_users, function(u){ u.dob = String(new Date(u.dob)); u.last_modified = String(new Date(u.last_modified)); return u; });
+        var cx = React.addons.classSet;
+        var classes = cx({
+          'result': true,
+          'form-inline': true,
+          'success': this.state.success,
+          'failure' : this.state.failure,
+          'normal' : !this.state.success && !this.state.failure
+        });
+
+        return (
+          <form className={classes} onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="accepting email" ref="email" className="input-box form-control"/>
+            <input type="submit" value="Query" className="btn btn-info"/>
+            <UserProfileTable profileData={this.state.result}/>
+          </form>
+        );
+    }
+});
+
+
 
 var SearchBox = React.createClass({
   render: function() {
     return (
-      <div className="searchBox">
+      <div className="resultBox">
         <h2>Search</h2>
-         <FilterableUserTable users={USERS} />
+        <SearchForm url={this.props.url} />
       </div>
     );
   }
 });
 
-React.renderComponent(<SearchBox  url="/search"/>, document.getElementById('search'));
+
+React.renderComponent(
+  <SearchBox url="/api/fetch_user" />,
+  document.getElementById('search')
+);
+
