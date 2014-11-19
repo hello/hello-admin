497ff93e832e10fe3b068fd62e5d3d92d4b4a4d2
longd3 <izzy3ds@gmail.com>
2014-11-18 21:24:09 -0800

    Add handy time converters

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,8 +1,13 @@
 import os
 import sys
 import logging as log
+from datetime import datetime
+import calendar
+import pytz
 
 
+utc_timezone = pytz.timezone("UTC")
+local_timezone = pytz.timezone("America/Los_Angeles")
 def display_error(e):
     """
     :param e: exception
@@ -15,3 +20,28 @@ def display_error(e):
         .replace("'", '').replace('"', '').strip()
     log.error(error)
     return error
+
+def iso_to_utc_timestamp(iso_time):
+    """
+    :param iso_time: a time string in ISO8601 format, e.g.: '2007-03-04T21:08:12'
+    :type iso_time: str
+    :return utc time in milliseconds
+    """
+    return calendar.timegm(datetime.strptime(iso_time.split('Z')[0], "%Y-%m-%dT%H:%M:%S").timetuple())
+
+
+def iso_to_datetime_obj(iso_time):
+    """
+    :param iso_time: a time string in ISO8601 format, e.g.: '2007-03-04T21:08:12'
+    :type iso_time: str
+    """
+    return datetime.strptime(iso_time.split('Z')[0], "%Y-%m-%dT%H:%M:%S").replace(tzinfo=utc_timezone).astimezone(local_timezone)
+
+
+def iso_to_human_timestring(iso_time):
+    """
+    :param iso_time: a time string in ISO8601 format, e.g.: '2007-03-04T21:08:12'
+    :type iso_time: str
+    """
+    return datetime.strptime(iso_time.split('Z')[0], "%Y-%m-%dT%H:%M:%S").strftime("%m-%d-%Y %H:%M:%S")
+
