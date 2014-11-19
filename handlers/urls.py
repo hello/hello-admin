12c84ecafa84998bd99a5d1e886eb2a30a6a19db
longd3 <izzy3ds@gmail.com>
2014-11-18 21:24:45 -0800

    Update page URLs

--- handlers/urls.py
+++ handlers/urls.py
@@ -1,49 +1,75 @@
 import webapp2
-
 import settings
-from handlers.setup import ProxyAPI, AppAPI, AppScopeAPI, RecentTokensAPI, CreateTokenAPI, \
-    CreateAccountAPI, CreateApplicationAgainstProdAPI, RegisterPillAPI, SetupAPI, UpdateAdminAccessTokenAPI
+from handlers.configuration import FeaturesAPI
+from handlers.cron import ZendeskCronHandler
+from handlers.teams import TeamsAPI
 from handlers.ext import ZendeskAPI
+from handlers.ext import ZendeskStatsAPI
 from handlers.firmware import FirmwareAPI
-from handlers.user import UserAPI
+from handlers.metrics import DebugLogAPI
 from handlers.metrics import PreSleepAPI
-from handlers.views import UserDashboardView, ChartHandler, SenseVisualView, SettingsView, UserView, ZendeskView \
-DebugLogView
+from handlers.setup import AppAPI
+from handlers.setup import AppScopeAPI
+from handlers.setup import CreateAccountAPI
+from handlers.setup import CreateApplicationAgainstProdAPI
+from handlers.setup import CreateTokenAPI
+from handlers.setup import ProxyAPI
+from handlers.setup import RecentTokensAPI
+from handlers.setup import RegisterPillAPI, SetupAPI
+from handlers.setup import UpdateAdminAccessTokenAPI
+from handlers.user import UserAPI
+from handlers.views import ChartHandler
+from handlers.views import ConfigurationView
+from handlers.views import DebugLogView
+from handlers.views import TeamsView
 from handlers.views import FirmwareView
-from handlers.metrics import PreSleepAPI, DebugLogAPI
-    
+from handlers.views import SenseVisualView
+from handlers.views import SettingsView
+from handlers.views import UserDashboardView
+from handlers.views import UserView
+from handlers.views import ZendeskView
 
 api_routes = [
     ('/api/app/?$', AppAPI),
     ('/api/app_scope/?$', AppScopeAPI),
     ('/api/debug_log/?$', DebugLogAPI),
-    ('/api/user/?$', UserAPI),
+    ('/api/device/?$', TeamsAPI),
+    ('/api/features/?$', FeaturesAPI),
+    ('/api/firmware/?$', FirmwareAPI),
     ('/api/presleep/?$', PreSleepAPI),
     ('/api/recent_tokens/?$', RecentTokensAPI),
+    ('/api/user/?$', UserAPI),
     ('/api/zendesk/?$', ZendeskAPI),
-    ('/api/firmware/add', FirmwareAPI),
+    ('/api/zendesk_stats/?$', ZendeskStatsAPI),
 ]
-
 
 page_routes = [
     ('/', UserView),
     ('/access_token', CreateTokenAPI),
     ('/charts', ChartHandler),
-    ('/create_account', CreateAccountAPI),
-    ('/debug_log/?$', DebugLogView),
-    ('/setup', SetupAPI),
     ('/create/app_against_prod', CreateApplicationAgainstProdAPI),
+    ('/create_account', CreateAccountAPI),
+    ('/configuration/?$', ConfigurationView),
+    ('/debug_log/?$', DebugLogView),
+    ('/teams/?$', TeamsView),
+    ('/firmware/?$', FirmwareView),
     ('/proxy/(.*)', ProxyAPI),
     ('/register_pill', RegisterPillAPI),
-    ('/update', UpdateAdminAccessTokenAPI),
-    ('/user_dashboard/?$', UserDashboardView),
     ('/sense/?$', SenseVisualView),
     ('/settings/?$', SettingsView),
+    ('/setup', SetupAPI),
+    ('/update', UpdateAdminAccessTokenAPI),
+    ('/user_dashboard/?$', UserDashboardView),
+    ('/users/?$', UserView),
     ('/zendesk/?$', ZendeskView),
-    ('/firmware/?$', FirmwareView),
+]
+
+
+cron_routes = [
+    ('/cron/zendesk_daily_stats/', ZendeskCronHandler)
 ]
 
 hello_admin_app = webapp2.WSGIApplication(
-    routes=api_routes + page_routes,
+    routes=api_routes + page_routes + cron_routes,
     debug=settings.DEBUG
 )
