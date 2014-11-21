c8510b641dc79895efc1295a8383e3e0900c0cbc
longd3 <izzy3ds@gmail.com>
2014-11-20 17:06:55 -0800

    Update routes

--- handlers/urls.py
+++ handlers/urls.py
@@ -2,8 +2,6 @@ import webapp2
 import settings
 from handlers.configuration import FeaturesAPI
 from handlers.cron import ZendeskCronHandler
-from handlers.teams import TeamsAPI
-from handlers.teams import Teams2API
 from handlers.ext import ZendeskAPI
 from handlers.ext import ZendeskStatsAPI
 from handlers.firmware import FirmwareAPI
@@ -13,34 +11,38 @@ from handlers.setup import AppAPI
 from handlers.setup import AppScopeAPI
 from handlers.setup import CreateAccountAPI
 from handlers.setup import CreateApplicationAgainstProdAPI
+from handlers.setup import CreateGroupsAPI
 from handlers.setup import CreateTokenAPI
 from handlers.setup import ProxyAPI
 from handlers.setup import RecentTokensAPI
 from handlers.setup import RegisterPillAPI, SetupAPI
 from handlers.setup import UpdateAdminAccessTokenAPI
+from handlers.teams import Teams2API
+from handlers.teams import TeamsAPI
 from handlers.user import UserAPI
 from handlers.views import ChartHandler
 from handlers.views import ConfigurationView
 from handlers.views import DebugLogView
-from handlers.views import TeamsView
+from handlers.views import ErrorView
 from handlers.views import FirmwareView
 from handlers.views import SenseVisualView
 from handlers.views import SettingsView
+from handlers.views import Teams2View
+from handlers.views import TeamsView
 from handlers.views import UserView
 from handlers.views import ZendeskView
-from handlers.views import Teams2View
-from handlers.views import ErrorView
 
 api_routes = [
     ('/api/app/?$', AppAPI),
     ('/api/app_scope/?$', AppScopeAPI),
+    ('/api/create_groups/?$', CreateGroupsAPI),
     ('/api/debug_log/?$', DebugLogAPI),
     ('/api/device/?$', TeamsAPI),
-    ('/api/teams2/?$', Teams2API),
     ('/api/features/?$', FeaturesAPI),
     ('/api/firmware/?$', FirmwareAPI),
     ('/api/presleep/?$', PreSleepAPI),
     ('/api/recent_tokens/?$', RecentTokensAPI),
+    ('/api/teams2/?$', Teams2API),
     ('/api/user/?$', UserAPI),
     ('/api/zendesk/?$', ZendeskAPI),
     ('/api/zendesk_stats/?$', ZendeskStatsAPI),
@@ -50,22 +52,22 @@ page_routes = [
     ('/', UserView),
     ('/access_token', CreateTokenAPI),
     ('/charts', ChartHandler),
+    ('/configuration/?$', ConfigurationView),
     ('/create/app_against_prod', CreateApplicationAgainstProdAPI),
     ('/create_account', CreateAccountAPI),
-    ('/configuration/?$', ConfigurationView),
     ('/debug_log/?$', DebugLogView),
-    ('/teams2/?$', TeamsView),
+    ('/error/?$', ErrorView),
     ('/firmware/?$', FirmwareView),
     ('/proxy/(.*)', ProxyAPI),
     ('/register_pill', RegisterPillAPI),
     ('/sense/?$', SenseVisualView),
     ('/settings/?$', SettingsView),
     ('/setup', SetupAPI),
+    ('/teams/?$', Teams2View),
+    ('/teams2/?$', TeamsView),
     ('/update', UpdateAdminAccessTokenAPI),
     ('/users/?$', UserView),
     ('/zendesk/?$', ZendeskView),
-    ('/teams/?$', Teams2View),
-    ('/error/?$', ErrorView),
 ]
 
 
