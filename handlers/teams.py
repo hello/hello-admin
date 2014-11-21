09fbb490642ccf6d42ac4c7b1ecbdac969c3e5e4
longd3 <izzy3ds@gmail.com>
2014-11-20 17:07:13 -0800

    Minor cleanup

--- handlers/teams.py
+++ handlers/teams.py
@@ -150,20 +150,3 @@ class Teams2API(FirmwareRequestHandler):
             log.error('ERROR: {}'.format(display_error(e)))
 
         self.response.write(json.dumps(output))
-
-    def post(self):
-        app_info_model = AppInfo(access_token=u'3.25666ca38dc94d31a4f91c1c2adcd37c', client_id=u'gae_admin', created=datetime.datetime(2014, 11, 18, 0, 39, 17, 997551), endpoint=u'https://dev-api.hello.is/v1/')
-        hello_service = OAuth2Service(
-            client_id=app_info_model.client_id,
-            client_secret='',
-            name='hello',
-            authorize_url=app_info_model.endpoint + 'oauth2/authorize',
-            access_token_url=app_info_model.endpoint + 'oauth2/token',
-            base_url=app_info_model.endpoint
-        )
-        session = hello_service.get_session(app_info_model.access_token)
-        headers = {'Content-Type': 'application/json'}
-        response = session.delete("teams/devices", data='{"name": "longtest", "ids": ["d22"]}', headers=headers)
-        # response = session.delete("teams/devices", data=json.dumps({"name": "longtest", "ids":["d15"]}), headers=headers)
-        print response
-        self.response.write(json.dumps({'data': [], 'error': ''}))
