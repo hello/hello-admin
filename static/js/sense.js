f7f54f990810204c530787fbc7386db25f7f7482
longd3 <izzy3ds@gmail.com>
2015-01-26 17:24:49 -0800

    Add sound to /sense charts

--- static/js/sense.js
+++ static/js/sense.js
@@ -8,7 +8,7 @@ var datepickerFormat = d3.time.format("%m/%d/%Y %I:%M:%S %p");
 var todayInDatepickerFormat = datepickerFormat(today);
 var lastWeekInDatepickerFormat = datepickerFormat(lastWeek);
 
-var sensorList = ['temperature', 'humidity', 'particulates', 'light'];
+var sensorList = ['temperature', 'humidity', 'particulates', 'light', 'sound'];
 var resolutionList = ['week', 'day'];
 var colorChoice = {
     temperature: {
@@ -26,12 +26,16 @@ var colorChoice = {
     light: {
         day: 'orange',
         week: 'brown'
+    },
+    sound: {
+        day: 'teal',
+        week: 'indigo'
     }
 };
 var legends = {
     day: 'every 5 minutes',
     week: 'every hour'
-}
+};
 var vizCanvas = React.createClass({
     componentDidMount: function() {
         console.log('sketch me up!')
@@ -40,112 +44,177 @@ var vizCanvas = React.createClass({
     render: function() {
 //        d3.selectAll("svg > *").remove();
         var that = this;
-        var temperatureChart, humidityChart, particulatesChart, lightChart;
-        nv.addGraph(function() {
-          temperatureChart = nv.models.lineChart()
-                        .margin({left: 75, right: 50})
-                        .useInteractiveGuideline(true)
-                        .transitionDuration(350)
-                        .showLegend(true)
-                        .showYAxis(true)
-                        .showXAxis(true);
+        var temperatureChart, humidityChart, particulatesChart, lightChart, soundChart;
+        if (that.props.temperature.length > 0) {
+            nv.addGraph(function () {
+                temperatureChart = nv.models.lineChart()
+                    .margin({left: 75, right: 50})
+                    .useInteractiveGuideline(true)
+                    .transitionDuration(350)
+                    .showLegend(true)
+                    .showYAxis(true)
+                    .showXAxis(true);
 
-          temperatureChart.xAxis
-              .axisLabel('Time')
-              .tickFormat(function(d) { return d3.time.format('%b %d %H:%M')(new Date(d)); });
+                temperatureChart.xAxis
+                    .axisLabel('Time')
+                    .tickFormat(function (d) {
+                        return d3.time.format('%b %d %H:%M')(new Date(d));
+                    });
 
-          temperatureChart.yAxis
-              .axisLabel('Temperature (°C )')
-              .tickFormat(function(d) {return d;});
-
-
-          d3.select('#temperature')
-              .datum(that.props.temperature)
-              .call(temperatureChart);
-          nv.utils.windowResize(function() { temperatureChart.update() });
-
-          return temperatureChart;
-        });
+                temperatureChart.yAxis
+                    .axisLabel('Temperature (°C )')
+                    .tickFormat(function (d) {
+                        return d;
+                    });
 
 
-        nv.addGraph(function() {
-          humidityChart = nv.models.lineChart()
-                        .margin({left: 75, right: 50})
-                        .useInteractiveGuideline(true)
-                        .transitionDuration(350)
-                        .showLegend(true)
-                        .showYAxis(true)
-                        .showXAxis(true);
-
-          humidityChart.xAxis
-              .axisLabel('Time')
-              .tickFormat(function(d) { return d3.time.format('%b %d %H:%M')(new Date(d)); });
-
-          humidityChart.yAxis
-              .axisLabel('Humidity (%)')
-              .tickFormat(function(d) {return d;});
+                d3.select('#temperature')
+                    .datum(that.props.temperature)
+                    .call(temperatureChart);
 
 
-          d3.select('#humidity')
-              .datum(that.props.humidity)
-              .call(humidityChart);
-          nv.utils.windowResize(function() { humidityChart.update() });
+                nv.utils.windowResize(function () {
+                    temperatureChart.update()
+                });
+                return temperatureChart;
+            });
+        }
 
-          return humidityChart;
+        if (that.props.humidity.length > 0) {
+            nv.addGraph(function () {
+                humidityChart = nv.models.lineChart()
+                    .margin({left: 75, right: 50})
+                    .useInteractiveGuideline(true)
+                    .transitionDuration(350)
+                    .showLegend(true)
+                    .showYAxis(true)
+                    .showXAxis(true);
 
-        });
+                humidityChart.xAxis
+                    .axisLabel('Time')
+                    .tickFormat(function (d) {
+                        return d3.time.format('%b %d %H:%M')(new Date(d));
+                    });
 
-        nv.addGraph(function() {
-          particulatesChart = nv.models.lineChart()
-                        .margin({left: 75, right: 50})
-                        .useInteractiveGuideline(true)
-                        .transitionDuration(350)
-                        .showLegend(true)
-                        .showYAxis(true)
-                        .showXAxis(true);
-
-          particulatesChart.xAxis
-              .axisLabel('Time')
-              .tickFormat(function(d) { return d3.time.format('%b %d %H:%M')(new Date(d)); });
-
-          particulatesChart.yAxis
-              .axisLabel('Particulates (µg/m³)')
-              .tickFormat(function(d) {return d;});
-
-
-          d3.select('#particulates')
-              .datum(that.props.particulates)
-              .call(particulatesChart);
-          nv.utils.windowResize(function() { particulatesChart.update() });
-
-          return particulatesChart;
-        });
-
-        nv.addGraph(function() {
-          lightChart = nv.models.lineChart()
-                        .margin({left: 75, right: 50})
-                        .useInteractiveGuideline(true)
-                        .transitionDuration(350)
-                        .showLegend(true)
-                        .showYAxis(true)
-                        .showXAxis(true);
-
-          lightChart.xAxis
-              .axisLabel('Time')
-              .tickFormat(function(d) { return d3.time.format('%b %d %H:%M')(new Date(d)); });
-
-          lightChart.yAxis
-              .axisLabel('Light (lm)')
-              .tickFormat(function(d) {return d;});
+                humidityChart.yAxis
+                    .axisLabel('Humidity (%)')
+                    .tickFormat(function (d) {
+                        return d;
+                    });
 
 
-          d3.select('#light')
-              .datum(that.props.light)
-              .call(lightChart);
-          nv.utils.windowResize(function() { lightChart.update() });
+                d3.select('#humidity')
+                    .datum(that.props.humidity)
+                    .call(humidityChart);
+                nv.utils.windowResize(function () {
+                    humidityChart.update()
+                });
 
-          return lightChart;
-        });
+                return humidityChart;
+            });
+        }
+
+        if (that.props.particulates.length > 0) {
+            nv.addGraph(function () {
+                particulatesChart = nv.models.lineChart()
+                    .margin({left: 75, right: 50})
+                    .useInteractiveGuideline(true)
+                    .transitionDuration(350)
+                    .showLegend(true)
+                    .showYAxis(true)
+                    .showXAxis(true);
+
+                particulatesChart.xAxis
+                    .axisLabel('Time')
+                    .tickFormat(function (d) {
+                        return d3.time.format('%b %d %H:%M')(new Date(d));
+                    });
+
+                particulatesChart.yAxis
+                    .axisLabel('Particulates (µg/m³)')
+                    .tickFormat(function (d) {
+                        return d;
+                    });
+
+
+                d3.select('#particulates')
+                    .datum(that.props.particulates)
+                    .call(particulatesChart);
+                nv.utils.windowResize(function () {
+                    particulatesChart.update()
+                });
+
+                return particulatesChart;
+            });
+        }
+
+        if (that.props.light.length > 0) {
+            nv.addGraph(function () {
+                lightChart = nv.models.lineChart()
+                    .margin({left: 75, right: 50})
+                    .useInteractiveGuideline(true)
+                    .transitionDuration(350)
+                    .showLegend(true)
+                    .showYAxis(true)
+                    .showXAxis(true);
+
+                lightChart.xAxis
+                    .axisLabel('Time')
+                    .tickFormat(function (d) {
+                        return d3.time.format('%b %d %H:%M')(new Date(d));
+                    });
+
+                lightChart.yAxis
+                    .axisLabel('Light (lm)')
+                    .tickFormat(function (d) {
+                        return d;
+                    });
+
+
+                d3.select('#light')
+                    .datum(that.props.light)
+                    .call(lightChart);
+                nv.utils.windowResize(function () {
+                    lightChart.update()
+                });
+
+                return lightChart;
+            });
+        }
+
+        if (that.props.sound.length > 0) {
+            nv.addGraph(function () {
+                soundChart = nv.models.lineChart()
+                    .margin({left: 75, right: 50})
+                    .useInteractiveGuideline(true)
+                    .transitionDuration(350)
+                    .showLegend(true)
+                    .showYAxis(true)
+                    .showXAxis(true);
+
+                soundChart.xAxis
+                    .axisLabel('Time')
+                    .tickFormat(function (d) {
+                        return d3.time.format('%b %d %H:%M')(new Date(d));
+                    });
+
+                soundChart.yAxis
+                    .axisLabel('Sound (dB)')
+                    .tickFormat(function (d) {
+                        return d;
+                    });
+
+
+                d3.select('#sound')
+                    .datum(that.props.sound)
+                    .call(soundChart);
+                nv.utils.windowResize(function () {
+                    soundChart.update()
+                });
+                return soundChart;
+            });
+        }
+
         var graphs = [];
         if (that.props.username !== "" && that.props.isUserKnown === false) {
           graphs.push(<div><p/><Alert bsStyle="danger">{"Unauthorized to see this user data!"}</Alert></div>);
@@ -229,6 +298,7 @@ var vizForm = React.createClass({
             humidity: [],
             particulates: [],
             light: [],
+            sound: [],
             impersonatees: [],
             username: "",
             isUserKnown: false
@@ -342,6 +412,7 @@ var vizForm = React.createClass({
                temperature={this.state.temperature}
                humidity={this.state.humidity}
                particulates={this.state.particulates}
+               sound={this.state.sound}
                light={this.state.light}
             />
         </div>)
