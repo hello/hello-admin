5b7ee1bb649ac3c3ffa3fb0238e1863f68abcd26
longd3 <izzy3ds@gmail.com>
2015-02-03 17:56:25 -0800

    More elegant sidebar
    @pims

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -2,30 +2,27 @@
 
 var SideBar = React.createClass({
    render: function() {
-       return (
-
-       <ListGroup>
-           <ListGroupItem href="/users">Users</ListGroupItem>
-           <ListGroupItem href="/zendesk_now">Zendesk Now</ListGroupItem>
-           <ListGroupItem href="/zendesk_history">Zendesk History</ListGroupItem>
-           <ListGroupItem href="/troubleshoot">Inactive Devices</ListGroupItem>
-           <ListGroupItem href="/key_store">Keystore Hints</ListGroupItem>
-           <ListGroupItem href="/keys">Keystore Provision</ListGroupItem>
-           <ListGroupItem href="/timeline">Timeline</ListGroupItem>
-           <ListGroupItem href="/sense">Room Data</ListGroupItem>
-           <ListGroupItem href="/battery">Pill Status</ListGroupItem>
-           <ListGroupItem href="/motion">Motion Data</ListGroupItem>
-           <ListGroupItem href="/alarms">Alarms Admin</ListGroupItem>
-           <ListGroupItem href="/debug_log">Sense Logs</ListGroupItem>
-           <ListGroupItem href="/application_logs">Application Logs</ListGroupItem>
-           <ListGroupItem href="/teams">Teams</ListGroupItem>
-           <ListGroupItem href="/configuration">Configuration</ListGroupItem>
-           <ListGroupItem href="/firmware">Firmware</ListGroupItem>
-           <ListGroupItem href="/pairing">Pairing</ListGroupItem>
-           <ListGroupItem href="/notification">Notification</ListGroupItem>
-           <ListGroupItem href="/label">Label</ListGroupItem>
-       </ListGroup>
-       )
+       return (<ListGroup>
+           <ListGroupItem bsStyle="default" href="/users">Users</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/timeline">Timeline</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/sense">Room Data</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/battery">Pill Status</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/motion">Motion Data</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/debug_log">Sense Logs</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/application_logs">Application Logs</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/label">Label Data</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/alarms">Alarms Admin</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/troubleshoot">Inactive Devices</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/key_store">Keystore Hints</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/keys">Keystore Provision</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/zendesk_now">Zendesk Now</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/zendesk_history">Zendesk History</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/teams">Teams</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/configuration">Configuration</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/firmware">Firmware</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/pairing">Pairing</ListGroupItem>
+           <ListGroupItem bsStyle="default" href="/notification">Notification</ListGroupItem>
+       </ListGroup>)
    }
 });
 
