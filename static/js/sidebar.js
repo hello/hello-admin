f088d11aa6e0e0b13606791ffdf2088a5360ee02
longd3 <izzy3ds@gmail.com>
2015-02-06 13:56:01 -0800

    Make sidebar group tile bolder

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -3,32 +3,32 @@
 var SideBar = React.createClass({
    render: function() {
        return (<ListGroup>
-           <ListGroupItem bsStyle="info">Home</ListGroupItem>
+           <ListGroupItem bsStyle="info" className="sidebar-group">Home</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/users">&#10149; Users</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/troubleshoot">&#10149; Inactive Devices</ListGroupItem>
 
-           <ListGroupItem bsStyle="info">Data</ListGroupItem>
+           <ListGroupItem bsStyle="info" className="sidebar-group">Data</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/timeline">&#10149; Timeline</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/sense">&#10149; Room Conditions</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/battery">&#10149; Pill Status</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/motion">&#10149; Motion</ListGroupItem>
 
-           <ListGroupItem bsStyle="info">Logs</ListGroupItem>
+           <ListGroupItem bsStyle="info" className="sidebar-group">Logs</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/debug_log">&#10149; Sense</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/application_logs">&#10149; Application</ListGroupItem>
 
-           <ListGroupItem bsStyle="info">Data Science</ListGroupItem>
+           <ListGroupItem bsStyle="info" className="sidebar-group">Data Science</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/label">&#10149; Label</ListGroupItem>
 
-           <ListGroupItem bsStyle="info">Keystore</ListGroupItem>
+           <ListGroupItem bsStyle="info" className="sidebar-group">Keystore</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/key_store">&#10149; Keystore Hints</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/keys">&#10149; Keystore Provision</ListGroupItem>
 
-           <ListGroupItem bsStyle="info">Zendesk</ListGroupItem>
+           <ListGroupItem bsStyle="info" className="sidebar-group">Zendesk</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/zendesk_now">&#10149; Zendesk Now</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/zendesk_history">&#10149; Zendesk History</ListGroupItem>
 
-           <ListGroupItem bsStyle="info">Operation</ListGroupItem>
+           <ListGroupItem bsStyle="info" className="sidebar-group">Operation</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/teams">&#10149; Teams</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/configuration">&#10149; Configuration</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/firmware">&#10149; Firmware</ListGroupItem>
@@ -36,7 +36,7 @@ var SideBar = React.createClass({
            <ListGroupItem bsStyle="default" href="/notification">&#10149; Notification</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/alarms">&#10149; Alarms</ListGroupItem>
 
-           <ListGroupItem bsStyle="info">Admin only</ListGroupItem>
+           <ListGroupItem bsStyle="info" className="sidebar-group">Admin only</ListGroupItem>
            <ListGroupItem bsStyle="default" href="/settings">&#10149; Settings</ListGroupItem>
        </ListGroup>)
    }
