2cdc3582e599a3f5091a8cecf93deac5385f3637
longd3 <izzy3ds@gmail.com>
2015-02-12 00:47:01 -0800

    Few small updates on /setup

--- settings.py
+++ settings.py
@@ -7,6 +7,9 @@ CLIENT_ID = 'gae'
 PROD_CLIENT = 'gae_admin'
 PROD_API = 'https://dev-api.hello.is/v1/'
 
+DEV_CLIENT = 'gae_admin'
+DEV_API = 'http://localhost:9999/v1/'
+
 if os.environ['SERVER_SOFTWARE'].find('Development') == 0:
     DEBUG = True
     CLIENT_ID = 'dev'
