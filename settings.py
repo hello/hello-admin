781f25c4bdac742276b5e4b3e7b59761b6d74aeb
longd3 <izzy3ds@gmail.com>
2015-03-10 14:56:02 -0700

    Update settings to grab credentials based on environment

--- settings.py
+++ settings.py
@@ -1,16 +1,29 @@
 import os
+from models.setup import AppInfo
+from models.setup import AdminUser
+from models.setup import UserGroup
+from models.ext import SearchifyCredentials
+from models.ext import ZendeskCredentials
+
 
 DEBUG = False
-ENVIRONMENT = 'prod'
-CLIENT_ID = 'sense-admin'
+ENVIRONMENT = "prod"
 
-PROD_CLIENT = 'sense-admin'
-PROD_API = 'https://api.hello.is/v1/'
+DEFAULT_LOCAL_DEV_CLIENT_ID = "gae_admin"
+DEFAULT_LOCAL_API_URL = "http://localhost:9999/v1/"
 
-DEV_CLIENT = 'gae_admin'
-DEV_API = 'http://localhost:9999/v1/'
+SERVER = os.environ.get("SERVER_NAME", "")
 
-if os.environ['SERVER_SOFTWARE'].find('Development') == 0:
+if SERVER.startswith("dev"):
+    ENVIRONMENT = "dev"
+
+if SERVER == "localhost" :
     DEBUG = True
-    CLIENT_ID = 'dev'
-    ENVIRONMENT = 'dev'
+    ENVIRONMENT = "local-dev"
+
+## Grab settings saved in GAE datastore
+APP_INFO = AppInfo.get_by_id(ENVIRONMENT)
+ADMIN_USER = AdminUser.get_by_id(ENVIRONMENT)
+SEARCHIFY = SearchifyCredentials.query().get()
+ZENDESK = ZendeskCredentials.query().get()
+USER_GROUP = UserGroup.query().get()
