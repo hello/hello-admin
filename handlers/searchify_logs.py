3f6c7a397da3a889ab41847833bc9c81253f98fd
longd3 <izzy3ds@gmail.com>
2015-03-19 16:24:13 -0700

    Refactor searchify handlers
    - Create a model for searchify query
    - All APIs extends from a single class SearchifyRequestHandler
    - Do not query from old index when looking up latest documents
    - When start time and end time are both unspecified, sort documents by
    latest first

--- handlers/searchify_logs.py
+++ handlers/searchify_logs.py
@@ -8,16 +8,15 @@ import settings
 __author__ = 'zet'
 
 
-class SenseLogsHandler(ProtectedRequestHandler):
+class SearchifyLogsHandler(ProtectedRequestHandler):
     """
     Retrieve debug logs
     """
-    def get_logs_by_index(self, index_name):
+    def get_logs_by_index(self, index_name, filters={}):
         output = {'data': [], 'error': ''}
 
-        max_results = int(self.request.get('max_results', default_value=20))
+        max_results = int(self.request.get('max_results', default_value=100))
         text_input = self.request.get('text', default_value="")
-        devices_input = self.request.get('devices', default_value="")
         start_time = self.request.get('start_time', default_value="")
         end_time = self.request.get('end_time', default_value="")
         searchify_cred = settings.SEARCHIFY
@@ -25,48 +24,31 @@ class SenseLogsHandler(ProtectedRequestHandler):
         try:
             if searchify_cred is None:
                 raise RuntimeError("Missing Searchify Credentials")
-            debug_log_api = ApiClient(searchify_cred.api_client)
 
-            index = debug_log_api.get_index('sense-logs')
-
-            if start_time.isdigit() and end_time.isdigit():
-                scoring_function = 'if((doc.var[0] - {})*(doc.var[0] - {}) < 0, doc.var[0], rel)'.format(start_time, end_time)
-            elif start_time.isdigit():
-                scoring_function = 'if((doc.var[0] - {}) > 0, doc.var[0], rel)'.format(start_time)
-            elif end_time.isdigit():
-                scoring_function = 'if((doc.var[0] - {}) < 0, doc.var[0], rel)'.format(end_time)
-            else:
-                scoring_function = 'doc.var[0]'
-
-            index.add_function(3, scoring_function)
-            search_params = {
-                'query': 'text:UART',
-                'fetch_fields': ['text', 'timestamp'],
-                'length': max_results,
-                'scoring_function': 3
-            }
+            index = ApiClient(searchify_cred.api_client).get_index(index_name)
+            searchify_query = SearchifyQuery()
 
             if text_input:
-                search_params['query'] = 'text:{}'.format(text_input)
-            else:
-                search_params['query'] = 'all:1'
+                searchify_query.set_query("text:{}".format(text_input))
 
-            devices_list = []
-            if devices_input:
-                input_list = stripStringToList(devices_input)
-                for d in input_list:
-                    if '@' in d and '.' in d:
-                        devices_list += self.hello_request(
-                            api_url="devices/q",
-                            type="GET",
-                            url_params={'email': d},
-                            test_mode=True
-                        ).data
-                    else:
-                        devices_list.append(d)
-                search_params['category_filters'] = {'device_id': devices_list}
+            if start_time.isdigit() or end_time.isdigit():  # Use custom scoring function if there is time input
+                if not end_time.isdigit():
+                    scoring_function = 'if((doc.var[0] - {}) > 0, doc.var[0], rel)'.format(start_time)
+                elif not start_time.isdigit():
+                    scoring_function = 'if((doc.var[0] - {}) < 0, doc.var[0], rel)'.format(end_time)
+                else:
+                    scoring_function = 'if((doc.var[0] - {})*(doc.var[0] - {}) < 0, doc.var[0], rel)'.format(start_time, end_time)
 
-            output['data'] = index.search(**search_params)['results']
+                index.add_function(3, scoring_function)
+                searchify_query.set_scoring_function(3)
+            elif '2015' not in index_name:
+                searchify_query.set_query("all:0") # Do not look for latest documents in the old index
+
+            if filters:
+                searchify_query.set_category_filters(filters)
+
+            searchify_query.set_length(max_results)
+            output['data'] = index.search(**searchify_query.mapping())['results']
 
         except Exception as e:
             output['error'] = display_error(e)
@@ -74,72 +56,41 @@ class SenseLogsHandler(ProtectedRequestHandler):
 
         return output
 
+    def get_logs_filtered_by_devices(self, index_name):
+        devices_input = self.request.get('devices', default_value="")
+        devices_list = []
+        if devices_input:
+            input_list = stripStringToList(devices_input)
+            for d in input_list:
+                if '@' in d and '.' in d:
+                    devices_list += self.hello_request(
+                        api_url="devices/q",
+                        type="GET",
+                        url_params={'email': d},
+                        test_mode=True
+                    ).data
+                else:
+                    devices_list.append(d)
+        if devices_list:  # Only filter if list of devices is not empty
+            return self.get_logs_by_index(index_name, {'device_id': devices_list})
 
-class SearchifyLogsHandler(ProtectedRequestHandler):
-    """
-    Retrieve application logs
-    """
+        return self.get_logs_by_index(index_name)
 
-    def get_logs_by_index(self, index_name):
-        output = {'data': [], 'error': ''}
-
-        max_results = int(self.request.get('max_results', default_value=20))
-        text_input = self.request.get('text', default_value="")
+    def get_logs_filtered_by_levels_orgins_versions(self, index_name):
         levels_input = self.request.get('levels', default_value="")
         origins_input = self.request.get('origins', default_value="")
         versions_input = self.request.get('versions', default_value="")
-        start_time = self.request.get('start_time', default_value="")
-        end_time = self.request.get('end_time', default_value="")
 
-        try:
-            searchify_cred = settings.SEARCHIFY
-            if not searchify_cred:
-                raise RuntimeError("Missing Searchify Credentials. Bailing.")
+        filters = {}
 
-            index = ApiClient(searchify_cred.api_client).get_index(index_name)
+        if levels_input:
+            filters.update({"level": stripStringToList(levels_input)})
+        if origins_input:
+            filters.update({"origin": stripStringToList(origins_input)})
+        if versions_input:
+            filters.update({"version": stripStringToList(versions_input)})
 
-            if start_time.isdigit() and end_time.isdigit():
-                scoring_function = 'if((doc.var[0] - {})*(doc.var[0] - {}) < 0, doc.var[0], rel)'.format(start_time, end_time)
-            elif start_time.isdigit():
-                scoring_function = 'if((doc.var[0] - {}) > 0, doc.var[0], rel)'.format(start_time)
-            elif end_time.isdigit():
-                scoring_function = 'if((doc.var[0] - {}) < 0, doc.var[0], rel)'.format(end_time)
-            else:
-                scoring_function = 'doc.var[0]'
-
-            index.add_function(3, scoring_function)
-            search_params = {
-                'query': 'text:data',
-                'category_filters': {},
-                'fetch_fields': ['text', 'timestamp'],
-                'length': max_results,
-                'scoring_function': 3
-            }
-
-            levels_list = []
-            if levels_input:
-                levels_list = stripStringToList(levels_input)
-                search_params['category_filters'].update({'level': levels_list})
-            if origins_input:
-                origins_list = stripStringToList(origins_input)
-                search_params['category_filters'].update({'origin': origins_list})
-            if versions_input:
-                versions_list = stripStringToList(versions_input)
-                search_params['category_filters'].update({'version': versions_list})
-
-            if text_input:
-                search_params['query'] = 'text:{}'.format(text_input)
-            else:
-                search_params['query'] = 'all:1'
-
-            output['data'] = index.search(**search_params)['results']
-
-        except Exception as e:
-            output['error'] = display_error(e)
-            log.error('ERROR: {}'.format(display_error(e)))
-
-        return output
-
+        return self.get_logs_by_index(index_name, filters)
 
 
 class ApplicationLogsAPI(SearchifyLogsHandler):
@@ -147,28 +98,29 @@ class ApplicationLogsAPI(SearchifyLogsHandler):
     Retrieve application logs
     """
     def get(self):
-        new_application_logs_output = self.get_logs_by_index("application-logs-2015-03")
-        old_application_logs_output = self.get_logs_by_index("application-logs")
+        new_application_logs_output = self.get_logs_filtered_by_levels_orgins_versions("application-logs-2015-03")
+        old_application_logs_output = self.get_logs_filtered_by_levels_orgins_versions("application-logs")
 
         aggregate_output = {
             'data': new_application_logs_output['data'] + old_application_logs_output['data'],
             'error': new_application_logs_output['error'] + old_application_logs_output['error'],
-        }
+            }
         self.response.write(json.dumps(aggregate_output))
 
 
-class SenseLogsAPI(SenseLogsHandler):
+class SenseLogsAPI(SearchifyLogsHandler):
     """
     Retrieve sense logs
     """
     def get(self):
-        new_sense_logs_output = self.get_logs_by_index("sense-logs-2015-03")
-        old_sense_logs_output = self.get_logs_by_index("sense-logs")
+
+        new_sense_logs_output = self.get_logs_filtered_by_devices("sense-logs-2015-03")
+        old_sense_logs_output = self.get_logs_filtered_by_devices("sense-logs")
 
         aggregate_output = {
             'data': new_sense_logs_output['data'] + old_sense_logs_output['data'],
             'error': new_sense_logs_output['error'] + old_sense_logs_output['error'],
-        }
+            }
         self.response.write(json.dumps(aggregate_output))
 
 
@@ -177,7 +129,7 @@ class WorkerLogsAPI(SearchifyLogsHandler):
     Retrieve worker logs
     """
     def get(self):
-        self.response.write(json.dumps(self.get_logs_by_index("workers-logs-2015-03")))
+        self.response.write(json.dumps(self.get_logs_filtered_by_levels_orgins_versions("workers-logs-2015-03")))
 
 
 class SearchifyStatsAPI(ProtectedRequestHandler):
@@ -198,3 +150,45 @@ class SearchifyStatsAPI(ProtectedRequestHandler):
         except Exception as e:
             output['error'] = display_error(e)
         self.response.write(json.dumps(output))
+
+class SearchifyQuery():
+    def __init__(self):
+        self.query = "all:1"  # by default search for all availalbe documents
+        self.fetch_fields = ['text', 'timestamp']  # by default just fetch text and timestamp field
+        self.category_filters = {}  # by default no filter
+        self.scoring_function = 0  # by default looks for latest documents
+        self.length = 100  # by default return at most 100 documents per search
+
+    def set_query(self, query):
+        if not isinstance(query, str):
+            raise TypeError("Query should be a string")
+        self.query = query
+
+    def set_fetch_fields(self, fetch_fields):
+        if not isinstance(fetch_fields, list):
+            raise TypeError("Fetch fields must be a list")
+        self.fetch_fields = fetch_fields
+
+    def set_category_filters(self, category_filters):
+        if not isinstance(category_filters, dict):
+            raise TypeError("Category filter must be a dict")
+        self.category_filters = category_filters
+
+    def set_scoring_function(self, scoring_function):
+        if not isinstance(scoring_function, int):
+            raise TypeError("Scoring function ID must an integer")
+        self.scoring_function = scoring_function
+
+    def set_length(self, length):
+        if not isinstance(length, int):
+            raise TypeError("Results length must an integer")
+        self.length = length
+
+    def mapping(self):
+        return {
+            'query': self.query,
+            'fetch_fields': self.fetch_fields,
+            'category_filters': self.category_filters,
+            'scoring_function': self.scoring_function,
+            'length': self.length
+        }
