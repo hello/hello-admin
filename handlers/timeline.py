40048333b7dcc33eabd0a8a88d9eccbc437d5e8a
longd3 <izzy3ds@gmail.com>
2015-03-20 14:22:01 -0700

    Add an invalidate-cache trigger on /timeline
    @pangw

--- handlers/timeline.py
+++ handlers/timeline.py
@@ -5,9 +5,19 @@ __author__ = 'zet'
 
 class TimelineAPI(ProtectedRequestHandler):
     def get(self):
+        """Retrieve user timeline"""
         email = self.request.get('email')
         date = self.request.get('date')
         self.hello_request(
             api_url="timeline/admin/{}/{}".format(email, date),
+            type="GET",
+        )
+
+    def post(self):
+        """Invalidate cache for user timeline"""
+        email = self.request.get('email')
+        date = self.request.get('date')
+        self.hello_request(
+            api_url="timeline/admin/invalidate/{}/{}".format(email, date),
             type="GET",
         )
