6195eca7ed24f50d2cead7caffc76cf061bec420
longd3 <izzy3ds@gmail.com>
2015-03-20 12:46:30 -0700

    Update environment switchers
    - Add indicator for local env
    - Fix https://github.com/hello/bugs/issues/116

--- static/js/base.js
+++ static/js/base.js
@@ -98,3 +98,10 @@ if (!Array.prototype.last){
     };
 }
 
+if (typeof String.prototype.startsWith != 'function') {
+  // see below for better implementation!
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) === 0;
+  };
+}
+
--- static/js/env_switcher.js
+++ static/js/env_switcher.js
@@ -2,34 +2,48 @@
 var Header = React.createClass({
     render: function() {
         var cx = React.addons.classSet;
-        var envProd, envDev;
-        if (document.URL.indexOf("dev") < 0) {
-            envProd = cx({
+        var envProd, envDev, localSwitcher=null, prodSwitcher=null, devSwitcher=null;
+        if (document.URL.startsWith("http://localhost")) {
+            envLocal = cx({
                 activeEnv: true,
                 env: true
-            });
-            envDev = cx({
-                activeEnv: false,
-                env: true
-            });
+            })
+            localSwitcher =  <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                <a className={envLocal} href={document.URL}> Local-Env </a>
+            </div>;
         }
         else {
-            envProd = cx({
-                activeEnv: false,
-                env: true
-            });
-            envDev = cx({
-                activeEnv: true,
-                env: true
-            });
+            if (!document.URL.startsWith("https://dev")) {
+                envProd = cx({
+                    activeEnv: true,
+                    env: true
+                });
+                envDev = cx({
+                    activeEnv: false,
+                    env: true
+                });
+            }
+            else {
+                envProd = cx({
+                    activeEnv: false,
+                    env: true
+                });
+                envDev = cx({
+                    activeEnv: true,
+                    env: true
+                });
+            }
+            prodSwitcher = <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                <a className={envProd} href="https://hello-admin.appspot.com/"> Prod-Env </a>
+            </div>;
+            devSwitcher =  <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                <a className={envDev} href="https://dev-dot-hello-admin.appspot.com/"> Dev-Env </a>
+            </div>;
         }
         return (<div>
-            <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
-            <a className={envProd} href="https://hello-admin.appspot.com/"> Prod-Env </a>
-        </div>
-        <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
-            <a className={envDev} href="https://dev-dot-hello-admin.appspot.com/"> Dev-Env </a>
-        </div>
+            {localSwitcher}
+            {prodSwitcher}
+            {devSwitcher}
         </div>)
     }
 });
