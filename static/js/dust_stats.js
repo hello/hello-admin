624d6670a5fdebbf61b92fd4bb1311198e2ed392
longd3 <izzy3ds@gmail.com>
2015-04-16 11:18:48 -0700

    Show dust stats for up to 3 senses at a time

--- static/js/dust_stats.js
+++ static/js/dust_stats.js
@@ -6,71 +6,82 @@ var c3Chart = React.createClass({
     },
     render: function() {
         var that = this, categories = ["variance", "max", "min", "average"], stackingGroups = [];
-        console.log('props', that.props.data);
-        var graphs = [<div id="graph" className="c3-chart"></div>];
 
-        c3.generate({
-            bindto: '#graph',
-            data: {
-                type: that.props.chartType,
-                json: that.props.data,
-                keys: {
-                    x: 'timestamp',
-                    value: categories
-                },
-                groups: stackingGroups,
-                axes: {
-                    variance: "y2"
-                },
-                colors: {
-                    average: "orangered",
-                    max: "#0D98BA",
-                    min: "green",
-                    variance: "violet"
-                }
-            },
-            axis: {
-                x: {
-                    tick: {
-                        format: function (x) { return d3.time.format('%b %d %H:%M')(new Date(x)); }
-                    },
-                    label: {
-                        text: "Time",
-                        position: 'outer-center'
-                    }
-                },
-                y: {
-                    label: {
-                        text: "Dust Count",
-                        position: 'outer-middle'
-                    }
-                },
-                y2: {
-                    show: true,
-                    label: {
-                        text: "Dust Count Variance",
-                        position: 'outer-middle'
-                    }
-                }
-            },
-            bar: {
-                width: {
-                    ratio: 0.4
-                }
-            },
-            grid: {
-                y: {
-                    show: false
-                }
-            },
-            zoom: {
-                enabled: that.props.zoomable
-            },
-            legend: {
-                position: "right"
-            }
+        if (that.props.data === {}) {
+            return <div/>;
+        }
+        var graphs = Array.apply(null, {length: 3}).map(Number.call, Number).map(function(i){
+            return <Row><div id={"graph" + i.toString()} className="c3-chart"></div></Row>;
         });
-
+        Object.keys(that.props.data).forEach(function(deviceId, i){
+            c3.generate({
+                bindto: "#graph" + i.toString(),
+                data: {
+                    type: that.props.chartType,
+                    json: that.props.data[deviceId],
+                    keys: {
+                        x: 'timestamp',
+                        value: categories
+                    },
+                    groups: stackingGroups,
+                    axes: {
+                        variance: "y2"
+                    },
+                    colors: {
+                        average: "orangered",
+                        max: "#0D98BA",
+                        min: "green",
+                        variance: "violet"
+                    }
+                },
+                axis: {
+                    x: {
+                        tick: {
+                            format: function (x) { return d3.time.format('%b %d %H:%M')(new Date(x)); }
+                        },
+                        label: {
+                            text: "Time",
+                            position: 'outer-center'
+                        }
+                    },
+                    y: {
+                        label: {
+                            text: "Count",
+                            position: 'outer-middle'
+                        }
+                    },
+                    y2: {
+                        show: true,
+                        label: {
+                            text: "Variance",
+                            position: 'outer-middle'
+                        }
+                    }
+                },
+                bar: {
+                    width: {
+                        ratio: 0.4
+                    }
+                },
+                grid: {
+                    y: {
+                        show: false
+                    }
+                },
+                zoom: {
+                    enabled: that.props.zoomable
+                },
+                legend: {
+                    position: "right"
+                }
+            });
+            d3.select("#graph" + i.toString() + " svg").append('text')
+                .attr('x', d3.select("#graph" + i.toString() + " svg").node().getBoundingClientRect().width / 2)
+                .attr('y', 16)
+                .attr('text-anchor', 'middle')
+                .style('font-size', '1.4em')
+                .text("Sense " + deviceId);
+            });
         return(<div>
             {graphs}
         </div>)
@@ -80,7 +91,7 @@ var c3Chart = React.createClass({
 var DustStatsChart = React.createClass({
     getInitialState: function() {
         return {
-            data: [],
+            data: {},
             zoomable: false,
             chartType: "line"
         }
@@ -113,58 +124,62 @@ var DustStatsChart = React.createClass({
 
     handleSubmit: function() {
         var that = this;
-        var deviceId = $('#device-id-input').val();
+        var deviceIds = $('#device-id-input').val().split(",");
         var startTs = $('#start-ts').val();
         var endTs = $('#end-ts').val();
         var length = $('#length').val();
         var startEpochMillis = startTs.isWhiteString() ? new Date().getTime()-3*24*3600000 : getUTCEpochFromLocalTime(startTs)*1000;
         var endEpochMillis = endTs.isWhiteString() ? new Date().getTime() : getUTCEpochFromLocalTime(endTs)*1000;
-        var requestData = {
-            device_id: deviceId,
-            start_ts: startEpochMillis,
-            end_ts: endEpochMillis,
-            length: length
-        };
-        $.ajax({
-            url: "/api/dust_stats",
-            type: "GET",
-            dataType: "json",
-            data: requestData,
-            success: function(response) {
-                console.log(response.data);
-                that.setState({data: filterData(response.data, startEpochMillis, endEpochMillis)});
-                that.pushHistory(deviceId, startTs, endTs, length);
-            }
+        that.pushHistory(deviceIds, startTs, endTs, length);
+        var aggData = {};
+        deviceIds.forEach(function(deviceId){
+            var requestData = {
+                device_id: deviceId.trim(),
+                start_ts: startEpochMillis,
+                end_ts: endEpochMillis,
+                length: length
+            };
+            $.ajax({
+                url: "/api/dust_stats",
+                type: "GET",
+                dataType: "json",
+                async: false,
+                data: requestData,
+                success: function(response) {
+                    aggData[deviceId.trim()] = filterData(response.data, startEpochMillis, endEpochMillis);
+                }
+            });
         });
+        that.setState({data: aggData});
         return false;
     },
 
     render: function() {
         return (<div>
             <form className="row" onSubmit={this.handleSubmit}>
-                <Col xs={3} sm={3} md={3}>
-                    <Input id="device-id-input" type="text" placeholder="Sense ID"/>
+                <Col xs={4} sm={4} md={4}>
+                    <LongTagsInput id="device-id-input" tagClass="label label-info" placeHolder="SenseIDs" />
                 </Col>
                 <LongDatetimePicker placeHolder="start ts" id="start-ts" size="3" />
                 <LongDatetimePicker placeHolder="end ts" id="end-ts" size="3" />
-                <Col xs={2} sm={2} md={2}>
-                    <Input id="length" type="text" placeholder="Data size"/>
+                <Col xs={1} sm={1} md={1}>
+                    <Input id="length" type="text" placeholder="Length"/>
                 </Col>
                 <Col xs={1} sm={1} md={1}>
                     <Button bsStyle="info" bsSize="large" className="btn-circle" type="submit"><Glyphicon glyph="search"/></Button>
                 </Col>
             </form>
+            <p className="chart-remark">Notes: <br/>
+                &nbsp;&nbsp;- This chart relies on logging consistency, {'regex_pattern = "collecting time (\d+)\\t.*?dust (\d+) (\d+) (\d+) (\d+)\\t"'}<br/>
+                &nbsp;&nbsp;- Max data size is 700, however for good performance, less than 200 points is reccommended<br/>
+                &nbsp;&nbsp;- Latest logs show up first, so you want to adjust end timestamp in case you want backtrack in further<br/>
+                &nbsp;&nbsp;- Can display data for up to 3 senses at once.
+            </p><br/>
             <Row>
                 <Col xs={12} sm={12} md={12}>
                     <c3Chart id="dust-stats-chart" data={this.state.data} zoomable={this.state.zoomable} chartType={this.state.chartType}/>
                 </Col>
             </Row>
-            <p className="chart-remark">Notes: <br/>
-            &nbsp;&nbsp;- This chart relies on logging consistency, {'regex_pattern = "collecting time (\d+)\\t.*?dust (\d+) (\d+) (\d+) (\d+)\\t"'}<br/>
-            &nbsp;&nbsp;- Max data size is 700, however for good performance, less than 200 points is reccommended<br/>
-            &nbsp;&nbsp;- Latest logs show up first, so you want to adjust end timestamp in case you want backtrack in further
-            </p>
-
         </div>)
     }
 });
