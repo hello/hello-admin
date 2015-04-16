7a8d6aeac63b0e65bc175e481a651088ecb42ca7
longd3 <izzy3ds@gmail.com>
2015-04-16 15:36:47 -0700

    Add a file exporter jsx template

---
+++ static/js/custom/file_exporter.js
@@ -0,0 +1,19 @@
+/** @jsx React.DOM */
+
+var FileExporter = React.createClass({
+    getDefaultProps: function(){
+        return {
+            fileContent: "",
+            icon: "cloud-download",
+            buttonName: "JSON",
+            fileName: "export.json"
+        }
+    },
+    render: function(){
+        return <a className="export" download={this.props.fileName} target="_blank"
+                href={"data:application/csv;charset=utf-8," + encodeURI(JSON.stringify(this.props.fileContent))}>
+                <Glyphicon glyph={this.props.icon}/> {this.props.buttonName}
+            </a>;
+    }
+});
+
