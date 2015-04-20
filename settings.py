6b83786051030da0c1b187913283cb842958643a
longd3 <izzy3ds@gmail.com>
2015-04-20 14:09:45 -0700

    Add admin against dev app info to settings

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ from google.appengine.api import memcache
 
 DEBUG = False
 ENVIRONMENT = "prod"
+ADMIN_APP_INFO = AppInfo.get_by_id("admin")
 
 DEFAULT_LOCAL_DEV_CLIENT_ID = "gae_admin"
 DEFAULT_LOCAL_API_URL = "http://localhost:9999/v1/"
@@ -17,14 +18,14 @@ SERVER = os.environ.get("SERVER_NAME", "")
 
 if SERVER.startswith("dev"):
     ENVIRONMENT = "dev"
+    ADMIN_APP_INFO = AppInfo.get_by_id("admin-dev")
 
-if SERVER == "localhost" :
+if SERVER == "localhost":
     DEBUG = True
     ENVIRONMENT = "local-dev"
 
 ## Grab settings saved in GAE memcache and only query from Datastore if they are not available
 APP_INFO = memcache.get("app_info"+ENVIRONMENT) or AppInfo.get_by_id(ENVIRONMENT)
-ADMIN_APP_INFO = AppInfo.get_by_id("admin")
 ADMIN_USER = memcache.get("admin_user"+ENVIRONMENT) or AdminUser.get_by_id(ENVIRONMENT)
 SEARCHIFY = memcache.get("searchify_credentials") or SearchifyCredentials.query().get()
 ZENDESK = memcache.get("zendesk_credentials") or ZendeskCredentials.query().get()
@@ -70,6 +71,8 @@ SEARCHIFY_LOGS_KEEP_DAYS = {
 }
 
 SEARCHIFY_PURGE_CAP_SIZE = 10000
+
+SEARCHIFY_PURGE_STATS_KEEP_DAYS = 7
 
 ACTIVE_DEVICES_KEEP_DAYS = 2
 
