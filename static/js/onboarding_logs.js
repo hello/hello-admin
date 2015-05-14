a7823f52ae9a3d2e6047a5b17a9f671c38cfc2df
longd3 <izzy3ds@gmail.com>
2015-05-13 17:20:09 -0700

    Improve sorting & filtering

--- static/js/onboarding_logs.js
+++ static/js/onboarding_logs.js
@@ -1,15 +1,15 @@
 var OnboardingLogs = React.createClass({
     getInitialState: function() {
         return {
-            data: [], error: "", limit: 10000,
+            originalData: [], data: [], error: "", limit: 10000,
             columns: [
-                {name: "ts_millis", title: "Timestamp", width: "100px"},
+                {name: "ts", title: "Time", width: "100px"},
                 {name: "sense_id", title: "Sense", width: "110px"},
                 {name: "pill_id", title: "Pill", width: "110px"},
-                {name: "account_id", title: "Account", width: "100px"},
-                {name: "result", title: "Result", width: "100px"},
-                {name: "action", title: "Action", width: "100px"},
-                {name: "ip", title: "IP Address", width: "100px"},
+                {name: "account_id", title: "Account", width: "115px"},
+                {name: "result", title: "Result", width: "115px"},
+                {name: "action", title: "Action", width: "115px"},
+                {name: "ip", title: "IP", width: "100px"},
                 {name: "info", title: "Info"}
             ],
             loading: false
@@ -31,7 +31,10 @@ var OnboardingLogs = React.createClass({
             success: function(response) {
                 console.log(response);
                 if (response.error.isWhiteString()) {
-                    that.setState({data: manipulateData(response.data)})
+                    that.setState({
+                        originalData: response.data,
+                        data: manipulateData(response.data)
+                    })
                 }
             }
         });
@@ -71,10 +74,14 @@ var OnboardingLogs = React.createClass({
             return null;
         }
         switch(sortInfo[0].name){
-            case "ts_millis": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareId) : this.state.data.sort(compareId).reverse()}); break;
-            case "name": this.setState({sortInfo: sortnfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareName) : this.state.data.sort(compareName).reverse()}); break;
-            case "accountEmail": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareEmail) : this.state.data.sort(compareEmail).reverse()}); break;
-            case "ts_millis": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareLastModified) : this.state.data.sort(compareLastModified).reverse()}); break;
+            case "info": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareInfo) : this.state.data.sort(compareInfo).reverse()}); break;
+            case "ip": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareIP) : this.state.data.sort(compareIP).reverse()}); break;
+            case "action": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareAction) : this.state.data.sort(compareAction).reverse()}); break;
+            case "result": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareResult) : this.state.data.sort(compareResult).reverse()}); break;
+            case "account_id": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareAccountId) : this.state.data.sort(compareAccountId).reverse()}); break;
+            case "sense_id": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareSenseId) : this.state.data.sort(compareSenseId).reverse()}); break;
+            case "pill_id": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(comparePillId) : this.state.data.sort(comparePillId).reverse()}); break;
+            case "ts": this.setState({sortInfo: sortInfo, data: sortInfo[0].dir === 1 ? this.state.data.sort(compareTimestamp) : this.state.data.sort(compareTimestamp).reverse()}); break;
             default: return null;
         }
 	},
@@ -121,8 +128,8 @@ var OnboardingLogs = React.createClass({
                 <Col xs={1}><Button type="submit"><Glyphicon glyph="search"/></Button></Col>
             </form>
             <form className="row" onSubmit={this.loadOnboardingLogsBySenseId}>
-                <Col xs={2}><Input type="text" id="sense-id"/></Col>
-                <Col xs={2}><Input type="text" id="count"/></Col>
+                <Col xs={2}><Input type="text" id="sense-id" placeholder="Sense ID" /></Col>
+                <Col xs={2}><Input type="text" id="count" placeholder="Count" /></Col>
                 <Col xs={1}><Button type="submit"><Glyphicon glyph="search"/></Button></Col>
             </form>
             <Row>
@@ -157,7 +164,58 @@ React.render(<OnboardingLogs />, document.getElementById("onboarding-logs"));
 
 function manipulateData(data) {
     return data.map(function(d){
-        d.ts_millis = d3.time.format("%b %d %H:%M")(new Date(d.ts_millis));
+        d.ts = d3.time.format("%b %d %H:%M")(new Date(d.ts_millis));
         return d;
     });
+}
+
+function compareNumber(e1, e2, attr) {
+    return e1['attr'] - e2['attr'];
+}
+
+function compareString(e1, e2, attr) {
+    if (e1[attr] < e2[attr]) {
+        return -1;
+    }
+    if (e1[attr] > e2[attr]) {
+        return 1;
+    }
+    return 0;
+}
+
+function compareAccountId(e1, e2) {
+    return compareNumber(e1, e2, "account_id");
+}
+
+function compareSenseId(e1, e2) {
+    return compareString(e1, e2, "sense_id");
+}
+
+function comparePillId(e1, e2) {
+    return compareString(e1, e2, "pill_id");
+}
+
+function compareAction(e1, e2) {
+    return compareString(e1, e2, "action");
+}
+
+function compareResult(e1, e2) {
+    return compareString(e1, e2, "result");
+}
+
+function compareInfo(e1, e2) {
+    return compareString(e1, e2, "info");
+}
+
+function compareTimestamp(e1, e2) {
+    return compareNumber(e1, e2, "ts_millis");
+}
+
+function compareIP(e1, e2) {
+    return compareNumber(evalIp(e1.ip), evalIp(e2.ip));
+}
+
+function evalIp(ipString) {
+    var ipSplit = ipString.split(".");
+    return Number(ipSplit[0])*256*256*256 + Number(ipSplit[1])*256*256 + Number(ipSplit[2])*256 + Number(ipSplit[3]);
 }
