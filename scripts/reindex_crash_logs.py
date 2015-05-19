a705240c123844ee3be0a1d12d737affbb1f7d68
longd3 <izzy3ds@gmail.com>
2015-05-19 10:24:08 -0700

    Skip reindexing the crash logs of previous job

--- scripts/reindex_crash_logs.py
+++ scripts/reindex_crash_logs.py
@@ -3,19 +3,35 @@ from indextank import ApiClient
 HOURS_BEFORE_CRASH = 2
 SENSE_LOGS_INDEX_NAME = "sense-logs-2015-05"
 FIRMWARE_CRASH_INDEX_NAME = "firmware-crash-logs"
-SEARCHIFY_ENDPOINT = "Ask Tim/Long"
-
-sc = ApiClient(SEARCHIFY_ENDPOINT)
+CRASH_KEYWORDS = ["travis", "fault", "xkd"]
+LAST_CRASH_TIMESTAMP = {
+    "travis": None,
+    "fault": None,
+    "xkd": None
+}
+LATEST_CRASH_TIMESTAMP = {}
+sc = ApiClient("http://:G3KvEnaw2bDdQc@d7q83.api.searchify.com/")
 slogs = sc.get_index(SENSE_LOGS_INDEX_NAME)
 fw_crash_index = sc.get_index(FIRMWARE_CRASH_INDEX_NAME)
 
 print "Begin extracting logs from {} to {}".format(SENSE_LOGS_INDEX_NAME, FIRMWARE_CRASH_INDEX_NAME)
 
-for crash_keyword in ["travis", "fault", "xkd"]:
-    device_crashes_count = slogs.search(
+added_docs_count = 0
+for crash_keyword in CRASH_KEYWORDS:
+    latest_crash = slogs.search(
         query="text:{}".format(crash_keyword),
-        length=1
-    )["facets"]['device_id']
+        length=1,
+        scoring_function=1,
+        fetch_variables=True,
+        docvar_filters={0: [[LAST_CRASH_TIMESTAMP[crash_keyword], None]]}
+    )
+    if latest_crash["matches"] == 0:
+        print "No more crash after {}".format(LAST_CRASH_TIMESTAMP[crash_keyword])
+        break
+    LATEST_CRASH_TIMESTAMP[crash_keyword] = latest_crash["results"][0]["variable_0"]
+    print "lastest crash time of {} is {}".format(crash_keyword, latest_crash["results"][0]["variable_0"])
+
+    device_crashes_count = latest_crash["facets"]['device_id']
     for k, v in device_crashes_count.items():
         crashes = slogs.search(
             query="text:{}".format(crash_keyword),
@@ -26,6 +42,8 @@ for crash_keyword in ["travis", "fault", "xkd"]:
         )
         for c in crashes['results']:
             start_ts = c["variable_0"] - 3600 * HOURS_BEFORE_CRASH
+            if LAST_CRASH_TIMESTAMP[crash_keyword]:
+                start_ts = max(start_ts, LAST_CRASH_TIMESTAMP[crash_keyword])
             end_ts = c["variable_0"]
             logs_until_the_crash = slogs.search(
                 query="device_id:{}".format(k),
@@ -41,16 +59,23 @@ for crash_keyword in ["travis", "fault", "xkd"]:
                     "timestamp": rr["timestamp"],
                     "text": rr["text"],
                     "device_id": rr["device_id"],
+                    "all": 1
                 },
                 "variables": {
                     0: rr["variable_0"]
                 },
                 "categories": {
-                    "device_id": rr["category_device_id"]
+                    "device_id": rr["category_device_id"],
+                    "crash_keyword": crash_keyword
                 }
             } for rr in logs_until_the_crash["results"]]
             try:
                 fw_crash_index.add_documents(docs)
+                print "Reindexed {} documents for device {} for {}".format(len(docs), k, crash_keyword)
+                added_docs_count += len(docs)
             except:
-                print "Failed to reindex logs of device {} for {}".format(k, crash_keyword)
-            print "Reindexed {} documents for device {} for {}".format(len(docs), k, crash_keyword)
+                print "Failed to add {} documents for device {} for {}".format(len(docs), k, crash_keyword)
+
+print "\nJob done. Aded {}".format(added_docs_count)
+print "Reindex crash logs until"
+print LATEST_CRASH_TIMESTAMP
