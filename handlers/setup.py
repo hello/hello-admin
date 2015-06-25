0b4f69b8e4c638f10f49dad03f64d8bcf0c7bc59
longd3 <izzy3ds@gmail.com>
2015-06-25 13:29:06 -0700

    Allow firmware group instead of super fw to see group status
    also update default groups

--- handlers/setup.py
+++ handlers/setup.py
@@ -427,11 +427,11 @@ class CreateGroupsAPI(ProtectedRequestHandler):
         groups_data = {
             'super_engineer': 'long@sayhello.com, tim@sayhello.com',
             'settings_moderator': 'pang@sayhello.com, chris@sayhello.com, kingshy@sayhello.com, jimmy@sayhello.com, km@sayhello.com',
-            'token_maker': 'pang@sayhello.com, chris@sayhello.com, kingshy@sayhello.com, josef@sayhello.com, jimmy@sayhello.com, km@sayhello.com, benjo@sayhello.com, jingyun@sayhello.com',
-            'customer_experience': 'marina@sayhello.com, tim@sayhello.com, chrisl@sayhello.com, natalya@sayhello.com, kenny@sayhello.com ',
+            'token_maker': 'pang@sayhello.com, chris@sayhello.com, kingshy@sayhello.com, josef@sayhello.com, jimmy@sayhello.com, km@sayhello.com, benjo@sayhello.com, jingyun@sayhello.com, kevin@sayhello.com',
+            'customer_experience': 'marina@sayhello.com, tim@sayhello.com, chrisl@sayhello.com, natalya@sayhello.com, kenny@sayhello.com , kevin@sayhello.com',
             'software': 'pang@sayhello.com, benjo@sayhello.com',
-            'hardware': 'scott@sayhello.com, ben@sayhello.com',
-            'firmware': 'chris@sayhello.com, josef@sayhello.com, tim@sayhello.com, pang@sayhello.com, jchen@sayhello.com, jimmy@sayhello.com, benjo@sayhello.com',
+            'hardware': 'ben@sayhello.com',
+            'firmware': 'chris@sayhello.com, josef@sayhello.com, tim@sayhello.com, pang@sayhello.com, jchen@sayhello.com, jimmy@sayhello.com, benjo@sayhello.com, kevin@sayhello.com',
             'super_firmware': 'chris@sayhello.com, josef@sayhello.com, tim@sayhello.com',
             'shipping': 'marina@sayhello.com, chrisl@sayhello.com, bryan@sayhello.com, natalya@sayhello.com, tim@sayhello.com, kingshy@sayhello.com, kenny@sayhello.com',
             'contractor': 'customersupport@sayhello.com'
