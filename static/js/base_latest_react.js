9c318f0a2c03f0fd9b891f87314cd69bc580eaa1
longd3 <izzy3ds@gmail.com>
2015-07-01 10:33:22 -0700

    Fix sidebar - part 1

--- static/js/base_latest_react.js
+++ static/js/base_latest_react.js
@@ -147,67 +147,6 @@ function luminate(hex, lum) {
 	return rgb;
 }
 
-var SideBar = React.createClass({
-   render: function() {
-       return (<ListGroup>
-           <ListGroupItem bsStyle="info" className="sidebar-group">Home</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/account_profile">&#x029D0; Account Profile</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/recent_accounts">&#x029D0; Recent Accounts</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/password_reset">&#x029D0; Password Reset</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/troubleshoot">&#x029D0; Inactive Devices</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/active_devices_history">&#x029D0; Devices Count</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/pch_serial_number_check">&#x029D0; Serial Check</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/users">&#x029D0; Users(Deperecated)</ListGroupItem>
-
-           <ListGroupItem bsStyle="info" className="sidebar-group">Metrics &amp; Diagnostics</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/users_inspection">&#x029D0; Users Inspection</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/sense_uptime">&#x029D0; Sense Uptime</ListGroupItem>
-
-           <ListGroupItem bsStyle="info" className="sidebar-group">Data</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/timeline">&#x029D0; Timeline</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/room_conditions">&#x029D0; Room Conditions</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/battery">&#x029D0; Pill Status</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/motion">&#x029D0; Motion</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/sense_events">&#x029D0; Sense Events</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/dust_stats">&#x029D0; Dust Statistics</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/alarm_rings_history">&#x029D0; Rings History</ListGroupItem>
-
-           <ListGroupItem bsStyle="info" className="sidebar-group">Logs</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/sense_logs">&#x029D0; Sense</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/onboarding_logs">&#x029D0; Onboarding</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/log_facets">&#x029D0; Facets Breakdown</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/black_list">&#x029D0; Black List</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/searchify_stats">&#x029D0; Statistics</ListGroupItem>
-
-
-           <ListGroupItem bsStyle="info" className="sidebar-group">Data Science</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/label">&#x029D0; Label</ListGroupItem>
-
-           <ListGroupItem bsStyle="info" className="sidebar-group">Keystore</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/key_store">&#x029D0; Keystore Hints</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/keys">&#x029D0; Keystore Provision</ListGroupItem>
-
-           <ListGroupItem bsStyle="info" className="sidebar-group">Zendesk</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/zendesk_now">&#x029D0; Zendesk Now</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/zendesk_history">&#x029D0; Zendesk History</ListGroupItem>
-
-           <ListGroupItem bsStyle="info" className="sidebar-group">Operation</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/teams">&#x029D0; Teams</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/configuration">&#x029D0; Configuration</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/firmware">&#x029D0; Firmware</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/pairing">&#x029D0; Pairing</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/notification">&#x029D0; Notification</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/alarms">&#x029D0; Alarms</ListGroupItem>
-
-           <ListGroupItem bsStyle="info" className="sidebar-group">Admin only</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/token_generator">&#x029D0; Token Generator</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/settings">&#x029D0; Settings</ListGroupItem>
-           <ListGroupItem bsStyle="default" href="/orders">&#x029D0; Orders</ListGroupItem>
-       </ListGroup>)
-   }
-});
-React.render(<SideBar />, document.getElementById('sidebar'));
-
 var Header = React.createClass({
     render: function() {
         var cx = React.addons.classSet;
