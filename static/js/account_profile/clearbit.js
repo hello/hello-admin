4a501ce8e5bde9a9e2f6b7694f05c1f75b1bdcb1
longd3 <izzy3ds@gmail.com>
2015-07-07 22:02:44 -0700

    Fix missing slash and make number presentation better

--- static/js/account_profile/clearbit.js
+++ static/js/account_profile/clearbit.js
@@ -24,8 +24,8 @@ var ClearbitInfo = React.createClass({
                 clearbitDetail.push(<Table>
                     <thead><tr><th/><th/></tr></thead>
                     <tbody>
-                        {clearbit.github.handle ? <tr><td>Github</td><td><a target="_blank" href={"https://github.com" + clearbit.github.handle}>{clearbit.github.handle}</a></td></tr> : null}
-                        {clearbit.github.followers ? <tr><td>Followers</td><td>{clearbit.github.followers}</td></tr> : null}
+                        {clearbit.github.handle ? <tr><td>Github</td><td><a target="_blank" href={"https://github.com/" + clearbit.github.handle}>{clearbit.github.handle}</a></td></tr> : null}
+                        {clearbit.github.followers ? <tr><td>Followers</td><td>{numberWithCommas(clearbit.github.followers)}</td></tr> : null}
                         {clearbit.github.company ? <tr><td>Company</td><td>{clearbit.github.company}</td></tr> : null}
                         {clearbit.github.blog ? <tr><td>Blog</td><td>{clearbit.github.blog}</td></tr> : null}
                         <tr><td/><td/></tr>
@@ -36,8 +36,8 @@ var ClearbitInfo = React.createClass({
                 clearbitDetail.push(<Table>
                     <thead><tr><th/><th/></tr></thead>
                     <tbody>
-                        {clearbit.twitter.handle ? <tr><td>Twitter</td><td><a target="_blank" href={"https://twitter.com" + clearbit.twitter.handle}>{clearbit.twitter.handle}</a></td></tr> : null}
-                        {clearbit.twitter.followers ? <tr><td>Followers</td><td>{clearbit.twitter.followers}</td></tr> : null}
+                        {clearbit.twitter.handle ? <tr><td>Twitter</td><td><a target="_blank" href={"https://twitter.com/" + clearbit.twitter.handle}>{clearbit.twitter.handle}</a></td></tr> : null}
+                        {clearbit.twitter.followers ? <tr><td>Followers</td><td>{numberWithCommas(clearbit.twitter.followers)}</td></tr> : null}
                         {clearbit.twitter.site ? <tr><td>Company</td><td>{clearbit.twitter.site}</td></tr> : null}
                         {clearbit.twitter.blog ? <tr><td>Blog</td><td>{clearbit.twitter.blog}</td></tr> : null}
                         <tr><td/><td/></tr>
@@ -48,7 +48,7 @@ var ClearbitInfo = React.createClass({
                 clearbitDetail.push(<Table>
                     <thead><tr><th/><th/></tr></thead>
                     <tbody>
-                        {clearbit.linkedin.handle ? <tr><td>Linkedin</td><td><a target="_blank" href={"https://linkedin.com" + clearbit.linkedin.handle}>{clearbit.linkedin.handle}</a></td></tr> : null}
+                        {clearbit.linkedin.handle ? <tr><td>Linkedin</td><td><a target="_blank" href={"https://linkedin.com/" + clearbit.linkedin.handle}>{clearbit.linkedin.handle}</a></td></tr> : null}
                         <tr><td/><td/></tr>
                     </tbody>
                 </Table>);
@@ -57,8 +57,8 @@ var ClearbitInfo = React.createClass({
                 clearbitDetail.push(<Table>
                     <thead><tr><th/><th/></tr></thead>
                     <tbody>
-                        {clearbit.angellist.handle ? <tr><td>AngelList</td><td><a target="_blank" href={"https://angel.co" + clearbit.angellist.handle}>{clearbit.angellist.handle}</a></td></tr> : null}
-                        {clearbit.angellist.followers ? <tr><td>Followers</td><td>{clearbit.angellist.followers}</td></tr> : null}
+                        {clearbit.angellist.handle ? <tr><td>AngelList</td><td><a target="_blank" href={"https://angel.co/" + clearbit.angellist.handle}>{clearbit.angellist.handle}</a></td></tr> : null}
+                        {clearbit.angellist.followers ? <tr><td>Followers</td><td>{numberWithCommas(clearbit.angellist.followers)}</td></tr> : null}
                         {clearbit.angellist.bio ? <tr><td>Bio</td><td>{clearbit.angellist.bio}</td></tr> : null}
                         {clearbit.angellist.blog ? <tr><td>Blog</td><td>{clearbit.angellist.blog}</td></tr> : null}
                         {clearbit.angellist.site ? <tr><td>Site</td><td>{clearbit.angellist.site}</td></tr> : null}
@@ -120,3 +120,7 @@ var ClearbitTile = React.createClass({
         </div>
     }
 });
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
