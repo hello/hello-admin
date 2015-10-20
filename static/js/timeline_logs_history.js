837b7e8d2db8550b8e15d7ff1b273c7ec5a94586
longd3 <izzy3ds@gmail.com>
2015-10-20 11:29:48 -0700

    First draft of /timeline_logs_history
    //TODOS:
    - add non-stack mode
    - better tooltips
    - normalize

--- static/js/timeline_logs_history.js
+++ static/js/timeline_logs_history.js
@@ -1,6 +1,66 @@
+var TimelineLogHistoryChart = React.createClass({
+    getDefaultProps: function() {
+        return {
+            id: "chart",
+            xTickFormat: "long",
+            chartType: "bar",
+            stackable: true
+        }
+    },
+    render: function() {
+        var that = this;
+        if(that.props.data && that.props.data.length > 0) {
+            var categories = Object.keys(that.props.data[0]).filter(function(d){return d !== "date" && d !== "timestamp"});
+            console.log(that.props.id, that.props.data);
+            var stackingGroups = that.props.stackable === true ? [categories] : [];
+
+            c3.generate({
+                bindto: '#'.concat(that.props.id),
+                data: {
+                    type: that.props.chartType,
+                    json: that.props.data,
+                    keys: {
+                        x: 'timestamp',
+                        value: categories
+                    },
+                    groups: stackingGroups
+                },
+                axis: {
+                    x: {
+                        tick: {
+                            format: function (x) {
+                                return d3.time.format('%b %d')(new Date(x));
+                            }
+                        }
+                    }
+                },
+                bar: {
+                    width: {
+                        ratio: 0.4
+                    }
+                },
+                grid: {
+                  y: {
+                      show: false
+                  }
+                },
+                zoom: {
+                    enabled: that.props.zoomable
+                },
+                legend: {
+                    position: "right"
+                }
+            });
+        }
+        return(
+            <div id={that.props.id} className="c3-chart"></div>
+        )
+    }
+});
+
 var TimelineLogsHistoryMaster = React.createClass({
     getInitialState: function() {
-        return {data: [], error: ""}
+        return {data: [], error: "", breakdownByAlgorithm: [], breakdownByError: [], breakdownByAlgorithmError: []}
     },
 
     pushHistory: function(startDate, endDate) {
@@ -42,17 +102,20 @@ var TimelineLogsHistoryMaster = React.createClass({
             data: {start_date: reformatDate(startDate), end_date: reformatDate(endDate)},
             type: "GET",
             success: function(response) {
-                response.aggregateData = getAggregateData(response.data);
-                this.setState(response);
-                console.log(response.aggregateData);
+                var aggregateData = getAggregateData(response.data);
+                console.log("aggregateData", aggregateData);
+                this.setState(aggregateData);
             }.bind(this)
         });
         return false;
     },
     render: function() {
         var alert = this.state.error ? <Alert>{this.state.error}</Alert> : null;
-        var results = this.state.data.length === 0 ? null :
-           "to be decided";
+        var results = [
+            <TimelineLogHistoryChart id="chart1" data={this.state.breakdownByAlgorithm} />,
+            <TimelineLogHistoryChart id="chart2" data={this.state.breakdownByError} />,
+            <TimelineLogHistoryChart id="chart3" data={this.state.breakdownByAlgorithmError} />
+        ];
 
         return (<div>
             <form onSubmit={this.handleSubmit}>
@@ -70,8 +133,6 @@ var TimelineLogsHistoryMaster = React.createClass({
 React.render(<TimelineLogsHistoryMaster dateId="date-input" />, document.getElementById("timeline-logs-history"));
 
 function getAggregateData(responseData) {
-    var aggegrateData = responseData.slice(0);
-
     var dateList = _.uniq(responseData.map(function(item){return item.date}));
     var algorithmList = _.uniq(responseData.map(function(item){return item.algorithm}));
     var errorList = _.uniq(responseData.map(function(item){return item.error}));
@@ -79,6 +140,7 @@ function getAggregateData(responseData) {
     var countByAlgoritmByDate = _.object(dateList, dateList.map(function(d){return {}}));
     var countByErrorByDate = _.object(dateList, dateList.map(function(d){return {}}));
     var countByDate = _.object(dateList, dateList.map(function(d){return 0}));
+
 
     responseData.forEach(function(item){
         dateList.forEach(function(d) {
@@ -97,13 +159,24 @@ function getAggregateData(responseData) {
             }
         });
     });
-    aggegrateData.map(function(item){
-        item.algorithmShare = 100 * item.count / countByAlgoritmByDate[item.date][item.algorithm];
-        item.errorShare = 100 * item.count / countByErrorByDate[item.date][item.error];
-        item.totalShare = 100 * item.count /countByDate[item.date];
-        return item;
+
+
+    var breakdown = {breakdownByAlgorithm: [], breakdownByError: [], breakdownByAlgorithmError: []};
+    dateList.forEach(function(d) {
+        var algorithmCount = {timestamp: new Date(Date.parse(d)).getTime(), date: d};
+        var errorCount = {timestamp: new Date(Date.parse(d)).getTime(), date: d};
+        var algorithmErrorCount = {timestamp: new Date(Date.parse(d)).getTime(), date: d};
+
+        responseData.filter(function(item){return item.date == d}).forEach(function(r){
+            algorithmCount[r.algorithm] = (algorithmCount[r.algorithm] || 0) + r.count;
+            errorCount[r.error] = (errorCount[r.error] || 0) + r.count;
+            algorithmErrorCount[r.algorithm + "-" + r.error] = (algorithmErrorCount[r.algorithm + "-" + r.error] || 0) + r.count;
+        });
+        breakdown.breakdownByAlgorithm.push(algorithmCount);
+        breakdown.breakdownByError.push(errorCount);
+        breakdown.breakdownByAlgorithmError.push(algorithmErrorCount);
     });
-    return aggegrateData;
+    return breakdown;
 }
 
 function reformatDate(dateString) {
