933219e60c45243ca9f1224b3eff3f53e2b6447e
longd3 <izzy3ds@gmail.com>
2015-10-27 12:18:51 -0700

    Use generated token rather than user/pw to query

--- api/elasticsearch.py
+++ api/elasticsearch.py
@@ -19,14 +19,12 @@ class SenseLogsElasticSearchAPI(ProtectedRequestHandler):
         ]
 
         response = requests.get(
-            url="{}/_search?{}".format(
+            url="{}:{}/_search?{}".format(
                 es_config.host,
+                es_config.http_port,
                 "&".join([esp for esp in es_params if esp != ""])
             ),
-            auth=HTTPBasicAuth(
-                username=es_config.read_user,
-                password=es_config.read_password
-            )
+            headers={"Authorization": es_config.token}
         )
 
         response_output = ResponseOutput(
