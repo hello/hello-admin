c073292fcac69a3972938cc9db80da7ccc429057
longd3 <izzy3ds@gmail.com>
2015-11-12 14:17:07 -0800

    Do not alert when total hits equal zero

--- cron/elasticsearch_cron.py
+++ cron/elasticsearch_cron.py
@@ -37,12 +37,13 @@ class FirmwareCrashElasticSearchAlert(ElasticSearchHandler):
                             "&start={}&end=&limit=&asc=false&crash_only=true".format(datetime.datetime.utcnow().strftime("%m/%d/%y %H:%M:%S"))
 
         total_hits = response_output.data.get("hits", {}).get("total", 0)
-        message = "{} documents with FW crash symptoms found <{}|last_hour>".format(total_hits, sense_logs_es_url)
-        aggregations = response_output.data.get("aggregations", {})
-        for agg_field in aggregations.keys():
-            message += "\n```Breakdown by {}\n".format(agg_field)
-            message += "\n".join(["{}: {}".format(j["key"].upper(), j["doc_count"]) for j in aggregations[agg_field]["buckets"]])
-            message += "\nOthers: {}\n".format(aggregations[agg_field]["sum_other_doc_count"])  + "\n```"
+        if total_hits > 0:
+            message = "{} documents with FW crash symptoms found <{}|last_hour>".format(total_hits, sense_logs_es_url)
+            aggregations = response_output.data.get("aggregations", {})
+            for agg_field in aggregations.keys():
+                message += "\n```Breakdown by {}\n".format(agg_field)
+                message += "\n".join(["{}: {}".format(j["key"].upper(), j["doc_count"]) for j in aggregations[agg_field]["buckets"]])
+                message += "\nOthers: {}\n".format(aggregations[agg_field]["sum_other_doc_count"])  + "\n```"
 
-        self.slack_pusher.send_to_firmware_crash_logs_channel(message)
+            self.slack_pusher.send_to_firmware_crash_logs_channel(message)
         self.response.write(response_output.get_serialized_output())
